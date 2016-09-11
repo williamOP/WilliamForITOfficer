61b56bbc75ad12ed2423ccf17029679aceaf7315
William Hong <william0088@gmail.com>
2016-09-11 11:30:46 +1000

    added carousel and links

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+$( document ).ready ( function() {
+    $(".carousel").slick ({
+        autoplay: true,
+        autoplaySpeed: 5000,  //int(ms)
+        dots: true,
+        pauseOnDotsHover: false,
+        speed: 500
+    });
+});
